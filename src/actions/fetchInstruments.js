7f805b57326d7c5986581063f3f7b9537ee10552
Alexander Rovang <wolf_vii@yahoo.com>
2021-07-09 12:16:01 -0400

    first commit

---
+++ src/actions/fetchInstruments.js
@@ -0,0 +1,10 @@
+export function fetchInstruments(){
+  return (dispatch) => {
+    fetch('http://127.0.0.1:3000/instruments')
+    .then(r=>r.json())
+    .then(instruments=> dispatch({
+      type: "FETCH_INSTRUMENTS", 
+      payload: instruments
+    }))
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import instrumentReducer from './reducers/instrumentReducer'
 
 ReactDOM.render(
   <React.StrictMode>
