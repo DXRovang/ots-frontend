c08bb70e8057dfd51870986fac4438e506e41d6f
Alexander Rovang <wolf_vii@yahoo.com>
2021-07-25 18:42:40 -0400

    adds create function

--- src/components/InstrumentForm.js
+++ src/components/InstrumentForm.js
@@ -9,137 +9,190 @@ import {
 class InstrumentForm extends React.Component{
 
   state = {
+    value: "",
+    user: "1",
     family: "",
-    value: "1",
-    category: ""
+    category: "",
+    maker: "",
+    price: "",
+    year: "",
+    frets: "",
+    material: "",
+    neck: "",
+    back: "",
+    bridge: "",
+    color: "",
+    hole: "",
+    description: "",
+    placeholder: "Select"
   }
 
   goBack(){
     this.props.history.goBack()
   }
-  handleFamilyChange = (e) =>{
+  handleChange = (e) =>{
     // debugger
     this.setState({
       [e.target.name]: e.target.value,
-      family: e.target.value,
+      // family: e.target.value,
     })
 
   }
-  handleCategoryChange = e =>{
-    this.setState({
-      [e.target.name]: e.target.value,
-      category: e.target.value,
-    })
-  }
+  // handleCategoryChange = e =>{
+  //   this.setState({
+  //     [e.target.name]: e.target.value,
+  //     category: e.target.value,
+  //   })
+  // }
+  // handleMakerChange = e =>{
+  //   this.setState({
+  //     [e.target.name]: e.target.value,
+  //     maker: e.target.value,
+  //   })
+  // }
 
   handleSubmit = (e) =>{
     e.preventDefault() 
     console.log("hey, hey")
-    // let formData = {
-    //   layout: this.state.layout
-    // }
-    // this.props.createInstruments(formData)
-    // this.props.history.push('/home')
+    let formData = {
+    user: this.state.user,
+    family: this.state.family,
+    category:this.state.category,
+    maker: this.state.maker,
+    price: this.state.price,
+    year: this.state.year,
+    description: this.state.description
+    }
+    this.props.createInstruments(formData)
+    this.props.history.push('/home')
   }
   render() {
     // debugger
     return(
       
-      <div className="title form"> 
+      <div className="formtitle form"> 
         <form onSubmit={e => this.handleSubmit(e)}>
-          <select onChange={this.handleFamilyChange}  name="value" value={this.state.value}>
-            <option value="1" disabled>Select</option>
+          <select onChange={this.handleChange}  name="family" value={this.state.family}>
+            <option default>Select</option>
             <option>Mandolin</option>
             <option>Guitar</option>
             <option>Banjo</option>
-          </select><br></br>
-          {/* <Category /> */}
+          </select>Family<br></br>
+
           {this.state.family === "Guitar" ? 
-          <select onChange={this.handleCategoryChange}  name="value" value={this.state.value} >
-            <option selected value="Select">Select</option>  
-            <option>6-string</option>
-            <option>12-string</option>
-            <option>Electric</option>
-            <option>Hollow-body</option>
-            <option>Semi-Hollow</option>
-            <option>Resonator</option>
-          </select> 
+            <>
+              <select onChange={this.handleChange}  name="category" value={this.state.category} >
+                <option default>Select</option>  
+                <option>6-string</option>
+                <option>12-string</option>
+                <option>Electric</option>
+                <option>Hollow-body</option>
+                <option>Semi-Hollow</option>
+                <option>Resonator</option>
+              </select>Category
+            </>
           : null }
           {this.state.family === "Mandolin" ? 
-          <select onChange={this.handleCategoryChange}  name="value" value={this.state.value} >
-            <option selected value="Select">Select</option>  
-            <option>F-style</option>
-            <option>A-style</option>
-          </select> 
+            <>
+              <select onChange={this.handleChange}  name="category" value={this.state.category} >
+                <option default>Select</option>  
+                <option>F-style</option>
+                <option>A-style</option>
+              </select>Category
+            </>
           : null }
           {this.state.family === "Banjo" ? 
-          <select onChange={this.handleCategoryChange}  name="value" value={this.state.value} >
-            <option selected value="Select" >Select</option>    
-            <option>4-string</option>
-            <option>5-string</option>
-          </select> 
+            <>
+              <select onChange={this.handleChange}  name="category" value={this.state.category} >
+                <option default >Select</option>    
+                <option>4-string</option>
+                <option>5-string</option>
+              </select>Category
+            </>
           : null }
-          {/* <input>Description</input> */}
           <br></br>
-          <label>
-            Price:
-            <input type="number" name="price"/>
-          </label>
-          <br></br>
-          <label>
-            Year:
-            <input type="number" name="year"/>
-          </label>
-          <br></br>
-          <label>
-            Maker:
-            <input type="text" name="maker"/>
-          </label>
-          <br></br>
-          <label>
-            Frets:
-            <input type="number" name="frets"/>
-          </label>
-          <br></br>          
-          <label>
-            Material:
-            <input type="text" name="material"/>
-          </label>
-          <br></br>          
-          <label>
-            SoundHole:
-            <input type="text" name="hole"/>
-          </label>
-          <br></br>          
-          <label>
-            Neck:
-            <input type="text" name="neck"/>
-          </label>
-          <br></br>          
-          <label>
-            Back:
-            <input type="text" name="back"/>
-          </label>
-          <br></br>          
-          <label>
-            Bridge:
-            <input type="text" name="bridge"/>
-          </label>
-          <br></br>
-          <label>
-            Color:
-            <input type="text" name="color"/>
-          </label>
-          <br></br>
-          <label>
-            Description:
-            <textarea name="description"/>
-          </label>
-          <br></br>
+          <div>
+            <label>           
+              <input type="number" name="price" onChange={this.handleChange} value={this.state.price}/>Price
+            </label><br></br>
+            <label>          
+              <input type="number" name="year" onChange={this.handleChange} value={this.state.year}/>Year
+            </label><br></br>      
+            {this.state.family === "Mandolin" ? 
+              <>
+                <label> 
+                  <select onChange={this.handleChange}  name="maker" value={this.state.maker} >
+                    <option default>Select</option>  
+                    <option>Weber</option>
+                    <option>Eastman</option>
+                    <option>Collings</option>
+                  </select> Maker
+                </label><br></br>
+              </>
+            : null }  
+            {this.state.family === "Guitar" ? 
+              <>
+                <label> 
+                  <select onChange={this.handleChange}  name="maker" value={this.state.maker} >
+                    <option default>Select</option>  
+                    <option>Fender</option>
+                    <option>Gibson</option>
+                    <option>Martin</option>
+                    <option>Taylor</option>
+                    <option>D'Angelico</option>
+                  </select> Maker
+                </label><br></br>
+              </>
+            : null }
+            {this.state.family === "Banjo" ? 
+              <>
+                <label> 
+                  <select onChange={this.handleChange}  name="maker" value={this.state.maker} >
+                    <option default>Select</option>  
+                    <option>Deering</option>
+                    <option>Pisgah</option>
+                    <option>Homemade</option>
+                  </select> Maker
+                </label><br></br>
+              </>
+            : null } 
+           
+            {/* <label>          
+              <input type="number" name="frets"/>Frets
+            </label><br></br>          
+            <label>        
+              <input type="text" name="material"/>Material
+            </label><br></br> 
+            {this.state.family === "Mandolin" ? 
+              <>   
+                <label>          
+                  <input type="text" name="hole"/>SoundHole
+                </label> <br></br>
+              </>
+            : null}
+            <label>         
+              <input type="text" name="neck"/>Neck
+            </label><br></br>          
+            <label>        
+              <input type="text" name="back"/>Back
+            </label><br></br>          
+            <label>          
+              <input type="text" name="bridge"/>Bridge
+            </label><br></br>
+            <label>       
+              <input type="text" name="color"/>Color
+            </label> */}
+
+            <br></br>
+            <br></br>
+            <label> 
+              <textarea name="description"/>Description
+            </label>
+            <br></br>
+          </div>
           <button>Add Instrument</button>
         </form>
         <Button onClick={() => this.goBack()}>Back</Button>
-        {this.state.family === "Guitar" ? <div>guitar</div> : null }
       </div>
     )}
   }
