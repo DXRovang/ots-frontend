1aa0df25ecbe7a5f5a441347dcfc9c8826ccb492
Alexander Rovang <wolf_vii@yahoo.com>
2021-08-27 12:20:24 -0400

    adds form to Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,14 +6,59 @@ import { Button } from 'reactstrap';
 
 class Login extends React.Component{
 
+  state = {
+    username: "",
+    email: "",
+    password: ""
+  }
+
   goBack(){
     this.props.history.goBack()
+  }
+
+  handleChange = (e) =>{
+    // debugger
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  handleSubmit = (e) =>{
+    e.preventDefault() 
+    console.log("hey, hey")
+    let formData = {
+    username: this.state.username,
+    email: this.state.email,
+    password:this.state.password,
+    }
+    this.props.createSession(formData)
+    this.props.history.push('/home')
   }
 
   render(){
     return(
       <>
-        <div className="text">Login Page.</div>
+        <div className="text">Login Page</div>
+
+        <div className="formtitle form">
+          <form onSubmit={e => this.handleSubmit(e)}>
+            <label>           
+              <input type="text" name="username" onChange={this.handleChange} value={this.state.username}/>Username
+            </label><br></br>
+            <label>           
+              <input type="text" name="email" onChange={this.handleChange} value={this.state.email}/>Email
+            </label><br></br>
+            <label>           
+              <input type="text" name="password" onChange={this.handleChange} value={this.state.password}/>Password
+            </label><br></br>
+          </form>
+        </div>
+
+
+
+
+
+
         <Button onClick={() => this.goBack()}>Back</Button>
       </>
     )
